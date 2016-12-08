29519490be873846f29bdccdd13054b7f91bb465
James Abney <james.abney@gmail.com>
2016-12-08 14:38:54 -0800

    Flesh out component details.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
+  // Apparently webpack has an issue with modueId: module.id.
+  // https://angular.io/docs/ts/latest/cookbook/component-relative-paths.html#!#webpack
+  // moduleId: module.id,
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  // With webpack, we can use the './' prefix to resolve template paths.
+  // (See above module.id note and link.)
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  // private heroService: HeroService;
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
