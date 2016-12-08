99684ef14dcad773501359801e9dcdd6d148e603
James Abney <james.abney@gmail.com>
2016-12-08 11:20:22 -0800

    Add initial RouterModule config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FormsModule }   from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
+
 
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
 
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
