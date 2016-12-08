3123083b5f57dbdfaefd44c8eb98fe8d395d246c
James Abney <james.abney@gmail.com>
2016-12-08 14:39:33 -0800

    Flesh out HeroDetailComponent.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'my-hero-detail',
@@ -14,7 +20,20 @@ import { Hero } from './hero';
     </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
   @Input()
   hero: Hero;
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero)
+  }
 }
