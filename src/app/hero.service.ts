6ca20ba8b345b05c151da4ddedea98899a42f33e
James Abney <james.abney@gmail.com>
2016-12-08 11:15:14 -0800

    Flesh out getHeroes method.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeros(): void {
-
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
   }
 }
