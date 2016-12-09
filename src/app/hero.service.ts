aff47787dff841dbe8ee605fc26098f72b2e5c0e
James Abney <james.abney@gmail.com>
2016-12-09 08:52:17 -0800

    Return hero from HEROMAP instead of searching array.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,7 +10,8 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    // return this.getHeroes()
+    //   .then(heroes => heroes.find(hero => hero.id === id));
+    return Promise.resolve(HEROMAP[id]);
   }
 }
