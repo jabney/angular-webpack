96dea1ef9abe6f55b66759ee3522cdd8d9a06e8a
James Abney <james.abney@gmail.com>
2016-12-11 18:35:25 -0800

    Don't load config based on environment, just load dev.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV == 'production') {
-    module.exports = require('./config/webpack.prod.js');
-} else {
-    module.exports = require('./config/webpack.dev.js');
-}
+module.exports = require('./config/webpack.dev.js');
