df7caa1c6b5e5cd13ddd0af4dc09f7547eec37f9
James Abney <james.abney@gmail.com>
2016-12-11 18:29:04 -0800

    Import rxjs extensions.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import './rxjs-extensions';
+
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
