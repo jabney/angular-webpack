a89f7c1ece6b66f10320d7007de2672909234e04
James Abney <james.abney@gmail.com>
2016-12-11 18:32:26 -0800

    Add hero-search imports, directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService }          from './hero.service';
 
 @NgModule({
@@ -30,7 +31,8 @@ import { HeroService }          from './hero.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
 
   providers: [ HeroService ],
