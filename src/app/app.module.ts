f28708bcb1246272c82a3473566317a25b634174
James Abney <james.abney@gmail.com>
2016-12-15 13:41:30 -0800

    Import CardComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { CardComponent } from './card.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   bootstrap: [ AppComponent ]
 })
