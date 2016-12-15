beccbd0dd06a47a916cd3cabe343571c82c3cb23
James Abney <james.abney@gmail.com>
2016-12-14 16:10:58 -0800

    Add Tabs and Tab components to declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs.component';
+import { TabComponent } from './tab.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TabComponent
   ],
   bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
