72c2400575b61470b526ebd0732367c455af3155
James Abney <james.abney@gmail.com>
2017-01-04 16:42:43 -0800

    Import NameParent and NameChild components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
 import { HeroParentComponent } from './hero-parent.component';
 import { HeroChildComponent } from './hero-child.component';
+
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
 
 @NgModule({
   imports: [
@@ -12,7 +15,9 @@ import { HeroChildComponent } from './hero-child.component';
   declarations: [
     AppComponent,
     HeroParentComponent,
-    HeroChildComponent
+    HeroChildComponent,
+    NameParentComponent,
+    NameChildComponent
   ],
   bootstrap: [ AppComponent ]
 })
