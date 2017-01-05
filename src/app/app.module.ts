d2146d9939389b58670147ef0ef7fd1ae6af06e7
James Abney <james.abney@gmail.com>
2017-01-04 16:11:44 -0800

    Add HeroParent and HeroChild components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { HeroParentComponent } from './hero-parent.component';
+import { HeroChildComponent } from './hero-child.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroParentComponent,
+    HeroChildComponent
   ],
   bootstrap: [ AppComponent ]
 })
