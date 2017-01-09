9bc95da4146ecd2a5ad8372ff472497be5273f00
James Abney <james.abney@gmail.com>
2017-01-09 15:05:53 -0800

    Add to project.

---
+++ src/app/countdown-vc-parent.component.ts
@@ -0,0 +1,14 @@
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountdownTimerComponent } from './countdown-timer.component';
+
+@Component({
+  selector: '',
+  template: ``
+})
+export class CountdownViewChildParentComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+
+  }
+}
