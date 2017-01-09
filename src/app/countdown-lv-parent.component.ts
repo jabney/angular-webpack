0adca1a54b3cc819acbd4bfc44ce6d976effd9db
James Abney <james.abney@gmail.com>
2017-01-09 09:27:05 -0800

    Implment CountdownLocalVariableParentComponent.

---
+++ src/app/countdown-lv-parent.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CountdownTimerComponent } from './countdown-timer.component';
+
+@Component({
+  selector: 'countdown-parent-lv',
+  template: `
+    <h3>Countdown to liftoff (via local variable)</h3>
+    <button (click)="timer.start()">Start</button>
+    <button (click)="timer.stop()">Stop</button>
+    <div class="seconds">{{timer.seconds}}</div>
+    <countdown-timer #timer></countdown-timer>
+  `,
+  styleUrls: ['demo.css']
+})
+export class CountdownParentLocalVariableComponent {
+
+}
