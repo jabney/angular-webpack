51149606b23c59990b33688e89964bf99ba5b46f
James Abney <james.abney@gmail.com>
2017-01-09 16:54:20 -0800

    Add to project.

---
+++ src/app/mission-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MissionService } from './mission.service';
+
+@Component({
+  selector: 'mission-control',
+  template: ``,
+  providers: [MissionService]
+})
+export class MissionControlComponent {
+
+}
