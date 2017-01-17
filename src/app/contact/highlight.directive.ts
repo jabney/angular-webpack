7f2ac09c54c383138ef15f08f0c36b887f90cb89
James Abney <james.abney@gmail.com>
2017-01-16 22:28:03 -0800

    Implement HighlightDirective.

---
+++ src/app/contact/highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[highlight], input'
+})
+export class highlightDirective {
+  constructor(el: ElementRef) {
+    el.nativeElement.style.backgroundColor = 'powderblue';
+    console.log(
+      `* Contact highlight called for ${el.nativeElement.tagName}`);
+  }
+}
