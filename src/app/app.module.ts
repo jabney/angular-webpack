d36f059a583a6f2b33e2b636ee549639020ef593
James Abney <james.abney@gmail.com>
2017-01-16 16:09:11 -0800

    Import TitleComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
+import { TitleComponent } from './title.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent, HighlightDirective ],
+  declarations: [
+    AppComponent,
+    HighlightDirective,
+    TitleComponent,
+  ],
   bootstrap: [ AppComponent ]
 })
 
