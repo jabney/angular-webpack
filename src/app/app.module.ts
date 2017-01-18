7d3ffe85939fa397c316894845ac7f5f59ba2112
James Abney <james.abney@gmail.com>
2017-01-18 09:56:42 -0800

    Remove FormsModule import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
+
 import { ContactModule } from './contact/contact.module';
 
-
 @NgModule({
-  imports: [ BrowserModule, FormsModule, ContactModule ],
+  imports: [ BrowserModule, ContactModule ],
   declarations: [AppComponent, HighlightDirective, TitleComponent],
   providers: [ UserService ],
   bootstrap: [ AppComponent ]
