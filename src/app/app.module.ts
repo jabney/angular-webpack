1438fe32680cafb3cd0fbb0da0e6d499e0e6cad8
James Abney <james.abney@gmail.com>
2017-01-17 17:32:37 -0800

    Import several components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
+import { AwesomePipe } from './contact/awesome.pipe';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact/contact.service';
+
+import {
+  HighlightDirective as ContactHighlightDirective
+} from './contact/highlight.directive';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [
     AppComponent,
     HighlightDirective,
     TitleComponent,
+
+    AwesomePipe,
+    ContactComponent,
+    ContactHighlightDirective,
   ],
-  providers: [UserService],
+  providers: [ UserService, ContactService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
