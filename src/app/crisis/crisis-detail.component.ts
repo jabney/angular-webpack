6cd0867bf52e9f004e8df7a809273e858f46899c
James Abney <james.abney@gmail.com>
2017-01-19 18:45:58 -0800

    Modify CrisisDetailComponent with editable name.

--- src/app/crisis/crisis-detail.component.ts
+++ src/app/crisis/crisis-detail.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute }    from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+import { Crisis, CrisisService } from './crisis.service';
 
 @Component({
   template: `
     <h3 highlight>Crisis Detail</h3>
-    <div>Crisis id: {{id}}</div>
+    <div *ngIf="crisis">
+      <div>Id: {{crisis.id}}</div><br>
+      <label>Name:
+        <input [(ngModel)]="crisis.name">
+      </label>
+    </div>
     <br>
     <a routerLink="../list">Crisis List</a>
   `
 })
 export class CrisisDetailComponent implements OnInit {
-  id: number;
-  constructor(private route: ActivatedRoute) {  }
+  crisis: Crisis;
+
+  constructor(
+    private route: ActivatedRoute,
+    private crisisService: CrisisService) {  }
 
   ngOnInit() {
-    this.id = parseInt(this.route.snapshot.params['id'], 10);
+    let id = parseInt(this.route.snapshot.params['id'], 10);
+    this.crisisService.getCrisis(id).then(crisis => this.crisis = crisis);
   }
 }
