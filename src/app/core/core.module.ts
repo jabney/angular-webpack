90c2f9bd39288c2a3ab42c9ebbb7ebfe3f82a4a7
James Abney <james.abney@gmail.com>
2017-01-19 18:45:22 -0800

    Implement CoreModule.

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import {
+  ModuleWithProviders,
+  NgModule,
+  Optional,
+  SkipSelf
+} from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from './title.component';
+import { UserService } from './user.service';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TitleComponent],
+  providers: [UserService],
+  exports: [TitleComponent]
+})
+export class CoreModule { }
