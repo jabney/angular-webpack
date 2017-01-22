d5be5472e0ac75e98f12f20ceae15459be7c7814
James Abney <james.abney@gmail.com>
2017-01-21 17:27:16 -0800

    UserService takes an optional config object.

--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -1,6 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
+
+let nextId = 1;
+
+export class UserServiceConfig {
+  userName = 'Philip Marlowe';
+}
 
 @Injectable()
 export class UserService {
+  id = nextId++;
   userName = 'Sherlock Holmes';
+
+  constructor(@Optional() config: UserServiceConfig) {
+    if (config) { this.userName = config.userName; }
+  }
+
+  getUsername() {
+    const suffix = this.id > 1 ? ` times ${this.id}` : '';
+    return this.userName + suffix;
+  }
 }
